e8ea36a8aab843b051e4a65d311433c72af0eca0
Manel <168196582+emanuelsousa123@users.noreply.github.com>
2025-05-27 19:35:51 -0300

    add movie_catalog.sql

---
+++ docs/movie_catalog.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 27/05/2025 às 22:12
+-- Versão do servidor: 9.1.0
+-- Versão do PHP: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `movie_catalog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `directors`
+--
+
+DROP TABLE IF EXISTS `directors`;
+CREATE TABLE IF NOT EXISTS `directors` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `users_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_director_user_id` (`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `directors_films`
+--
+
+DROP TABLE IF EXISTS `directors_films`;
+CREATE TABLE IF NOT EXISTS `directors_films` (
+  `films_id` int NOT NULL,
+  `directors_id` int NOT NULL,
+  KEY `fk_directorsfilms_director_id` (`directors_id`),
+  KEY `fk_directorsfilms_film_id` (`films_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `films`
+--
+
+DROP TABLE IF EXISTS `films`;
+CREATE TABLE IF NOT EXISTS `films` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `synopsis` text,
+  `review` text,
+  `image_url` varchar(255) DEFAULT NULL,
+  `year` int DEFAULT NULL,
+  `review_score` int DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `users_id` int NOT NULL,
+  `genres_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_films_user_id` (`users_id`),
+  KEY `fk_films_genre_id` (`genres_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `genres`
+--
+
+DROP TABLE IF EXISTS `genres`;
+CREATE TABLE IF NOT EXISTS `genres` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `users_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_genres_user_id` (`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `lists`
+--
+
+DROP TABLE IF EXISTS `lists`;
+CREATE TABLE IF NOT EXISTS `lists` (
+  `id` int NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `users_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_lists_users_id` (`users_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `lists_films`
+--
+
+DROP TABLE IF EXISTS `lists_films`;
+CREATE TABLE IF NOT EXISTS `lists_films` (
+  `lists_id` int NOT NULL,
+  `films_id` int NOT NULL,
+  KEY `fk_listsfilms_lists_id` (`lists_id`),
+  KEY `fk_listsfilms_films_id` (`films_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `directors`
+--
+ALTER TABLE `directors`
+  ADD CONSTRAINT `fk_director_user_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `directors_films`
+--
+ALTER TABLE `directors_films`
+  ADD CONSTRAINT `fk_directorsfilms_director_id` FOREIGN KEY (`directors_id`) REFERENCES `directors` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_directorsfilms_film_id` FOREIGN KEY (`films_id`) REFERENCES `films` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `films`
+--
+ALTER TABLE `films`
+  ADD CONSTRAINT `fk_films_genre_id` FOREIGN KEY (`genres_id`) REFERENCES `genres` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_films_user_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `genres`
+--
+ALTER TABLE `genres`
+  ADD CONSTRAINT `fk_genres_user_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `lists`
+--
+ALTER TABLE `lists`
+  ADD CONSTRAINT `fk_lists_users_id` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+--
+-- Restrições para tabelas `lists_films`
+--
+ALTER TABLE `lists_films`
+  ADD CONSTRAINT `fk_listsfilms_films_id` FOREIGN KEY (`films_id`) REFERENCES `films` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `fk_listsfilms_lists_id` FOREIGN KEY (`lists_id`) REFERENCES `lists` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
