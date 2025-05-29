29ef78d451ebc27714a923359a5a54a6b06491b6
Manel <168196582+emanuelsousa123@users.noreply.github.com>
2025-05-29 20:41:47 -0300

    Delete movie_catalog directory

--- movie_catalog/actions/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();             // Inicia a sessão
-    session_unset();             // Limpa todas as variáveis da sessão
-    session_destroy();           // Destroi a sessão
-
-    // Redireciona para a página de login (ou inicial)
-    header("Location: ../index.php");
-    exit;
-?>
