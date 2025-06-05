6c591b44cfe7406d8d11b54f0042ddb349973869
Manel <168196582+emanuelsousa123@users.noreply.github.com>
2025-06-04 23:48:26 -0300

    mudandado o nome do fechar_popup.js

---
+++ movie_catalog/assets/js/open_close_popup.js
@@ -0,0 +1,14 @@
+function pen_popup(type) {
+    document.getElementById("dark-background").style.display = 'flex';
+    document.getElementById(type).style.display = 'flex';
+}
+
+function close_popup(type) {
+    document.getElementById("dark-background").style.display = 'none';
+    document.getElementById(type).style.display = 'none';
+}
+
+function open_close(type1,type2) {
+    document.getElementById(type1).style.display = 'flex';
+    document.getElementById(type2).style.display = 'none';
+}
